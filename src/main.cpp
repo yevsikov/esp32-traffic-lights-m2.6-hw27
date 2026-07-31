a4894ae00c9cd616952d455b56d3fc88f0144610
vladislavyevsikov <vladislav.yevsikov@beetweb.com>
2026-07-31 14:15:48 +0300

    Add traffic light logic via delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,56 @@
 #include <Arduino.h>
 
+#define LED_RED_PIN 5
+#define LED_YELLOW_PIN 6
+#define LED_GREEN_PIN 7
+#define BTN_PIN 8
+#define BAUDRATE 115200
+
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(BAUDRATE);
+
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
+
+  // init traffic lighter via yellow blinking
+  digitalWrite(LED_YELLOW_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_YELLOW_PIN, LOW);
+  delay(500);
+  digitalWrite(LED_YELLOW_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_YELLOW_PIN, LOW);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  delay(500);
+  digitalWrite(LED_YELLOW_PIN, LOW);
+  digitalWrite(LED_RED_PIN, HIGH);
+  delay(5000);
+  digitalWrite(LED_YELLOW_PIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_RED_PIN, LOW);
+  delay(250);
+  digitalWrite(LED_YELLOW_PIN, LOW);
+  digitalWrite(LED_GREEN_PIN, HIGH);
+  delay(7000);
+  digitalWrite(LED_GREEN_PIN, LOW);
+  delay(500);
+  digitalWrite(LED_GREEN_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_GREEN_PIN, LOW);
+  delay(500);
+  digitalWrite(LED_GREEN_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_GREEN_PIN, LOW);
+  delay(500);
+  digitalWrite(LED_YELLOW_PIN, HIGH);
+  delay(1000);
+
+
 }
 
